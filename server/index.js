377e3f0ef6653c0e2aa06faabb8ea46ccd4508ea
Your Name <youremail@example.com>
2024-12-28 09:43:38 +0200

    adding signup and login css files

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ port = 3001
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     credentials:true,
 }))
 
